81296755e37b02aea90dc7cd9994dee4d2a88d9a
Christopher Rathgeb <crathgeb@digitova.com>
2017-08-21 16:15:13 -0400

    Added TabNavigatorIcon and NavigationIcon exports to module primary index

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 export {default as GridView} from "./GridView"
 export {default as Icon} from "./Icon"
 export {default as Menu} from "./Menu"
+export {default as TabNavigatorIcon} from "./ReactovaNavigator/TabNavigator/TabNavigatorIcon"
+export {default as NavigationIcon} from "./Icon/NavigationIcon"
 export {default as ReactovaNavigator} from "./ReactovaNavigator"
