705c5d1790250a80cd74614c3fa1544d448e780a
Christopher Rathgeb <crathgeb@digitova.com>
2017-08-21 17:18:25 -0400

    Made Navigator Pull Theme from props if included or default if not.

--- src/ReactovaNavigator/index.js
+++ src/ReactovaNavigator/index.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import buildRouteConfig from './RouteConfigurator'
 import createNavigator from './NavigatorFactory'
+import DefaultTheme from'../../config/theme'
 
 export default class extends Component {
 	render() {
 		const routeConfig = buildRouteConfig(this.props.navigationSchema);
-		const Navigator = createNavigator(this.props.navigationSchema,routeConfig);
+		const Navigator = createNavigator(this.props.navigationSchema,routeConfig, this.getTheme());
 		return <Navigator {...this.props}/>
+	}
+
+	getTheme(){
+		return this.props.hasOwnProperty('theme') ? this.props.theme : DefaultTheme
 	}
 }
