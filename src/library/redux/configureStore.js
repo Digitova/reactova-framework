f14a4676bd8dc8853239def7110a692785ad7e06
Christopher Rathgeb <crathgeb@digitova.com>
2017-08-22 10:48:17 -0400

    Clean up refactor references.

--- src/library/redux/configureStore.js
+++ src/library/redux/configureStore.js
@@ -2,14 +2,14 @@ import { createLogger } from 'redux-logger';
 import { combineReducers } from 'redux';
 import { default as thunk } from 'redux-thunk';
 import { createStore, applyMiddleware, compose } from 'redux';
-import ReactovaReducers from '../../reducers'
+import ReactovaReducers from '../../reducers/_reducers'
 
 const loggerMiddleware = createLogger({ predicate: (getState, action) => __DEV__ });
 
 export default function configureStore(initialState, reducers) {
 
-	const combinedReducers = combineReducers({ReactovaReducers: ReactovaReducers, AppReducers: reducers})
-	
+	const combinedReducers = combineReducers({reactova: ReactovaReducers, app: reducers})
+
 	const enhancer = compose(
 		applyMiddleware(
 			thunk,
