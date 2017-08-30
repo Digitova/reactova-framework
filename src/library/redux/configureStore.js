30d0b0510b0a2dc75342613304f95c854ea046be
Christopher Rathgeb <crathgeb@digitova.com>
2017-08-30 12:08:19 -0400

    adjusted configureStore to include onHydrationComplete for redux-persist

--- src/library/redux/configureStore.js
+++ src/library/redux/configureStore.js
@@ -1,12 +1,16 @@
 import { createLogger } from 'redux-logger';
 import { combineReducers } from 'redux';
+import {AsyncStorage} from 'react-native'
 import { default as thunk } from 'redux-thunk';
+import { NavigationActions } from 'react-navigation'
 import { createStore, applyMiddleware, compose } from 'redux';
+
+import {persistStore, autoRehydrate, purgeStoredState} from 'redux-persist'
 import ReactovaReducers from '../../reducers/_reducers'
 
 const loggerMiddleware = createLogger({ predicate: (getState, action) => __DEV__ });
 
-export default function configureStore(initialState, appReducers, navigationReducers) {
+export default function configureStore(initialState, appReducers, navigationReducers, onHydrationComplete) {
 
 	const combinedReducers = combineReducers({reactova: ReactovaReducers, app: appReducers, nav: navigationReducers})
 
@@ -14,8 +18,22 @@ export default function configureStore(initialState, appReducers, navigationRedu
 		applyMiddleware(
 			thunk,
 			loggerMiddleware,
-		)
+		),
+
+		autoRehydrate()
 	);
-	return createStore(combinedReducers, enhancer)
+	const store = createStore(combinedReducers, enhancer);
+
+	persistStore(store, {storage: AsyncStorage, blacklist: ['nav']},() => {
+		onHydrationComplete(store)
+		console.log("hydration complete");
+	});
+
+	// purgeStoredState({storage: AsyncStorage}).then(() => {
+	// 	console.log('purge completed')
+	// }).catch(() => {
+	// 	console.log('purge of someReducer failed')
+	// })
+	return store;
 }
 
