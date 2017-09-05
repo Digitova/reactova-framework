c56786c272582950c024c1faa345705d153e0306
Christopher Rathgeb <crathgeb@digitova.com>
2017-09-05 16:00:14 -0400

    remove jwtdecode for now because it uses window.atob which is not in the react nnative runtime

--- src/library/jwt/decodeJwt.js
+++ src/library/jwt/decodeJwt.js
@@ -1,6 +1,7 @@
 
 export default function decodeJwt(token){
-	var base64Url = token.split('.')[1];
+	return token;
+/*	var base64Url = token.split('.')[1];
 	var base64 = base64Url.replace('-', '+').replace('_', '/');
-	return JSON.parse(window.atob(base64));
+	return JSON.parse(window.atob(base64));*/
 }
