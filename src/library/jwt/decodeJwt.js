4a310d5a35bb9e86656c4f17661ca4b978e219bf
Christopher Rathgeb <crathgeb@digitova.com>
2017-09-05 22:54:12 -0400

    added jwt-decode.

--- src/library/jwt/decodeJwt.js
+++
@@ -1,7 +0,0 @@
-
-export default function decodeJwt(token){
-	return token;
-/*	var base64Url = token.split('.')[1];
-	var base64 = base64Url.replace('-', '+').replace('_', '/');
-	return JSON.parse(window.atob(base64));*/
-}
