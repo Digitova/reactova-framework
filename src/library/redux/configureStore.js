0ae4ffc994443ba8df73faa04df835202f7f42e2
Christopher Rathgeb <crathgeb@digitova.com>
2018-02-23 06:19:54 -0500

    added redux middleware for react-navigation

--- src/library/redux/configureStore.js
+++ src/library/redux/configureStore.js
@@ -7,6 +7,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { composeWithDevTools } from 'remote-redux-devtools';
 import {persistStore, autoRehydrate, purgeStoredState} from 'redux-persist'
 import ReactovaReducers from '../../reducers/_reducers'
+import {createReactNavigationReduxMiddleware} from "react-navigation-redux-helpers";
 
 import * as capitalizeFirstLetter from '../strings/capitalizeFirstLetter'  // do not remove.
 
@@ -15,9 +16,15 @@ const loggerMiddleware = createLogger({ predicate: (getState, action) => __DEV__
 export default function configureStore(initialState, appReducers, devToolOptions, onHydrationComplete, persistanceBlacklist) {
 	const combinedReducers = combineReducers(Object.assign(ReactovaReducers, appReducers))
 	const composeEnhancers = composeWithDevTools({name: devToolOptions.instanceName})
+	const reactNavigationMiddleware = createReactNavigationReduxMiddleware(
+		"root",
+		state => state.nav,
+	);
+
 	const enhancer = composeEnhancers(
 		applyMiddleware(
 			thunk,
+			reactNavigationMiddleware,
 			loggerMiddleware,
 		),
 		autoRehydrate()
