41d5cf99a57013525504556765109b52b555dacc
Christopher Rathgeb <crathgeb@digitova.com>
2018-02-25 19:36:21 -0500

    fixed theming of StackHeader

--- src/components/Navigation/StackHeader.js
+++ src/components/Navigation/StackHeader.js
@@ -9,15 +9,15 @@ class StackHeader extends Component
 		const { theme } = this.props
 		return <Header
 			{...this.props}
-			style={{
-				backgroundColor: theme.navigation,
-				paddingTop: 0,
-				height: 50
-			}}
 
 			getScreenDetails={(scene) => {
 				return deepmerge(this.props.getScreenDetails(scene),{
 					options: {
+						headerStyle:{
+							backgroundColor: theme.navigation,
+							paddingTop: 0,
+							height: 50
+						},
 						headerTintColor: theme.navigationAccent,
 						headerTitleStyle: {
 							color: theme.navigationText
