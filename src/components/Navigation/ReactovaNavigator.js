6923ed54ce033722f20e3fafab67258b5a412d96
Anthony Garritano <anthony@anthonygarritano.com>
2018-03-09 10:11:32 -0500

    Allow LoadingScreen to be optional and not default

--- src/components/Navigation/ReactovaNavigator.js
+++ src/components/Navigation/ReactovaNavigator.js
@@ -3,28 +3,33 @@ import { TabNavigator } from 'react-navigation';
 import BaseNavigator from './BaseNavigator'
 
 
-const ReactovaNavigator = (navigationSchema,LoadingScreen) => {
+const ReactovaNavigator = (navigationSchema, LoadingScreen = null, ladingScreenIsDefaultRoute = true) => {
+	let loading = {}
+	const baseNavigatorName = 'BaseNavigator'
+	let initialRouteName = baseNavigatorName
+	if(LoadingScreen) {
+		loading = {
+			Loading: {
+				screen: LoadingScreen
+			}
+		}
+		initialRouteName = ladingScreenIsDefaultRoute ? 'Loading' : initialRouteName
+	}
+
 	const routerRouteConfig = {
-		Loading: {
-			screen: LoadingScreen,
-		},
-		BaseNavigator: {
+		...loading,
+		[baseNavigatorName]: {
 			path: 'base',
 			screen: BaseNavigator(navigationSchema),
 		},
 	}
 	const routerNavigationConfig = {
-		initialRouteName: 'Loading',
+		initialRouteName,
 		navigationOptions:{
 			tabBarVisible: false
 		}
 	}
-
 	return TabNavigator(routerRouteConfig,routerNavigationConfig);
 };
 
 export default ReactovaNavigator;
-
-
-
-
